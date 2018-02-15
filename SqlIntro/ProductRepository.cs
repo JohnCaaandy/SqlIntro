c07f7485693fcb0eec9f5331cfc0fb1d27e88b86
Jack Mentzer <jackdanielmentzer@gmail.com>
2018-02-15 14:25:21 -0500

    Refactored full ProductRepository, added new class, made IDbConnection

--- SqlIntro/ProductRepository.cs
+++ SqlIntro/ProductRepository.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Runtime.InteropServices;
@@ -11,11 +12,11 @@ namespace SqlIntro
 {
     public class ProductRepository
     {
-        private readonly string _connectionString;
+        private readonly IDbConnection _conn;
 
-        public ProductRepository(string connectionString)
+        public ProductRepository(IDbConnection conn)
         {
-            _connectionString = connectionString;
+            _conn = conn;
         }
         /// <summary>
         /// Reads all the products from the products table
@@ -23,63 +24,74 @@ namespace SqlIntro
         /// <returns></returns>
         public IEnumerable<Product> GetProducts()
         {
-            using (var conn = new MySqlConnection(_connectionString))
+            using (var conn = _conn)
             {
                 conn.Open();
 
                 var cmd = conn.CreateCommand();
-                cmd.CommandText = "SELECT * FROM product"; 
+                cmd.CommandText = "SELECT Name FROM product";
+
                 var dr = cmd.ExecuteReader();
                 while (dr.Read())
                 {
-                    yield return new Product { Name = dr["Name"].ToString() };
-                }
-            }
-        }
-
-        /// <summary>
-        /// Deletes a Product from the database
-        /// </summary>
-        /// <param name="id"></param>
-        public void DeleteProduct(int id)
-        {
-            using (var conn = new MySqlConnection(_connectionString))
-            {
-                var cmd = conn.CreateCommand();
-                cmd.CommandText = ""; //Write a delete statement that deletes by id
-                cmd.ExecuteNonQuery();
-            }
-        }
-        /// <summary>
-        /// Updates the Product in the database
-        /// </summary>
-        /// <param name="prod"></param>
-        public void UpdateProduct(Product prod)
-        {
-            //This is annoying and unnecessarily tedious for large objects.
-            //More on this in the future...  Nothing to do here..
-            using (var conn = new MySqlConnection(_connectionString))
-            {
-                var cmd = conn.CreateCommand();
-                cmd.CommandText = "update product set name = @name where id = @id";
-                cmd.Parameters.AddWithValue("@name", prod.Name);
-                cmd.Parameters.AddWithValue("@id", prod.Id);
-                cmd.ExecuteNonQuery();
-            }
-        }
-        /// <summary>
-        /// Inserts a new Product into the database
-        /// </summary>
-        /// <param name="prod"></param>
-        public void InsertProduct(Product prod)
-        {
-            using (var conn = new MySqlConnection(_connectionString))
-            {
-                var cmd = conn.CreateCommand();
-                cmd.CommandText = "INSERT into product (name) values(@name)";
-                cmd.Parameters.AddWithValue("@name", prod.Name);
-                cmd.ExecuteNonQuery();
+                    yield return new Product
+                    {
+                        Name = dr["Name"].ToString(),
+                        Id = int.Parse(dr["ProductId"].ToString())
+                };
             }
         }
     }
+
+    /// <summary>
+    /// Deletes a Product from the database
+    /// </summary>
+    /// <param name="id"></param>
+    public void DeleteProduct(int id)
+    {
+        using (var conn = _conn)
+        {
+            conn.Open();
+
+            var cmd = conn.CreateCommand();
+            cmd.CommandText = "DELETE FROM product WHERE ProductId= @id";
+            cmd.AddParamWithValue("@id", id);
+            cmd.ExecuteNonQuery();
+        }
+    }
+    /// <summary>
+    /// Updates the Product in the database
+    /// </summary>
+    /// <param name="prod"></param>
+    public void UpdateProduct(Product prod)
+    {
+
+        using (var conn = _conn)
+        {
+
+            conn.Open();
+
+            var cmd = conn.CreateCommand();
+            cmd.CommandText = "update product set name = @name where id = @id";
+
+            cmd.AddParamWithValue("@name", prod.Name);
+            cmd.AddParamWithValue("@id", prod.Id);
+            cmd.ExecuteNonQuery();
+        }
+    }
+    /// <summary>
+    /// Inserts a new Product into the database
+    /// </summary>
+    /// <param name="prod"></param>
+    public void InsertProduct(Product prod)
+    {
+        using (var conn = _conn)
+        {
+            var cmd = conn.CreateCommand();
+            cmd.CommandText = "INSERT into product (name) values(@name)";
+            cmd.AddParamWithValue("@name", prod.Name);
+            cmd.ExecuteNonQuery();
+        }
+    }
+}
 }
